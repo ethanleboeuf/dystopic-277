b92c2b9b6614f8e9bc9b13e9002b895ced71383c
ethanleboeuf <ethanl20@vt.edu>
2019-03-04 02:01:36 -0800

    Added patrol.jl
    Created patrol.jl which is Joe's initial patrol implementation and
    plotting functions.

---
+++ patrol.jl
@@ -0,0 +1,134 @@
+using Plots, Colors, PyCall, Compose
+pyplot()
+include("graph_environment.jl")
+# This code simulates a patrol of a grid where expected commute time
+# averaged over all pairs of nodes is minimized.
+
+"""
+    weighted_adjacency(A)
+
+Takes in Adjacency matrix and outputs the altered weighted adjacent matrices
+"""
+function weighted_adjacency(A)
+
+    # FUTURE IMPLEMENTATION: Will take A in, use convex to get the weighting, and
+    # and then go on. For now, will just read it in.
+
+    # Graph with Optimal edge probabilities
+    M = read_graph("C:\\Users\\Ethan\\Documents\\GitHub\\dystopic-277\\weightsLarge.csv")
+    n = nv(M)
+    k = n^(1/2);
+
+    # this is the weighted adjacency matrix
+    weightedA = zeros(n,n)
+    for edge in edges(M)
+        weightedA[src(edge),dst(edge)] = get_prop(M,edge,:weight)
+        weightedA[dst(edge),src(edge)] = get_prop(M,edge,:weight)
+    end
+
+    # This alters the weighted adjacency matrix so it is easier to randomly
+    # sample an edge later. Sometimes the rows don't add exactly to 1 because
+    # of the rounding. If you get a running error, this is why. Rerun and it
+    # should be fine.
+    weightedB = zeros(n,n);
+    for i = 1:n
+       previous = 0
+       for j = 1:n
+           if weightedA[i, j] != 0
+               weightedB[i,j] = previous + weightedA[i, j]
+               previous = weightedB[i, j]
+           end
+       end
+    end
+    return weightedB
+end
+
+"""
+    find_path(time,startNode,A)
+
+Takes in amount of time to simulate, t, the initial node, node0, and the
+adjacency matrix, A, and outputs the calculated travel path
+"""
+function find_path(time_end,startNode,A)
+    #Standin
+    M = read_graph("C:\\Users\\Ethan\\Documents\\GitHub\\dystopic-277\\weightsLarge.csv")
+    # weightedB = weighted_adjacency(A) I think we can get rid of needing
+    # weighted_adjacency completely
+    n = nv(M)
+    k = sqrt(n)
+    # path in node form
+    Path = zeros(time_end,1);
+    Path[1] = startNode;
+    for t = 2:time_end
+        x = rand();
+        sumcheck = 0
+        num_neighbors = length(neighbors(M,Int(Path[t-1])))
+        prob_choices = zeros(num_neighbors,1)
+        for neighbor in enumerate(neighbors(M,Int(Path[t-1])))
+            prob_choices[neighbor[1]] = get_prop(M,Int(Path[t-1]),neighbor[2],:weight)
+            if x < sum(prob_choices[1:neighbor[1]])
+                Path[t] = neighbor[2]
+                break
+            end
+        end
+       #  for j = 1:n
+       #      if x < weightedB[Int(Path[t-1]), j]
+       #          Path[t] = j
+       #          break
+       #     end
+       # end
+    end
+
+    # path in x and y coordinates
+    travel = zeros(time_end,2);
+    for i = 1:length(Path)
+        if mod(Path[i],k) != 0
+            travel[i,1] = k*(Path[i]/k - floor(Path[i]/k));
+            travel[i,2] = ceil(Path[i]/k)
+        else
+            travel[i,1]  = k;
+            travel[i,2] = ceil(Path[i]/k)
+        end
+    end
+    return travel
+end
+"""
+    plot_path(travel,fn,A)
+
+Plots the travel path given and saves it to the given file location
+"""
+function plot_path(travel,fn,A)
+    # M stuff is temp
+    M = read_graph("C:\\Users\\Ethan\\Documents\\GitHub\\dystopic-277\\weightsLarge.csv")
+    n = nv(M)
+    k = sqrt(n)
+    time_end = size(travel,1)
+    finalplot = plot(legend=false)
+    title!("Patrolling of Robots")
+    xlabel!("x position")
+    ylabel!("y position")
+    plot!(collect(1:(k-1)/99:k), ones(100,1), color = :black)
+    plot!(collect(1:(k-1)/99:k), ones(100,1)*k, color = :black)
+    plot!(ones(100,1), collect(1:(k-1)/99:k), color = :black)
+    plot!(ones(100,1)*k, collect(1:(k-1)/99:k), color = :black)
+    for ii = 1:time_end-1
+        line_color = weighted_color_mean(ii/time_end, colorant"black", colorant"red")
+        plot!(travel[ii:ii+1,1], travel[ii:ii+1,2],color = line_color)
+    end
+    # annotate!(k+5,0,text("this is #5", 16, :red, :center))
+    png(finalplot,fn)
+    return finalplot
+end
+
+"""
+    main
+
+I don't believe in the concept of "main" functions, but whatever.
+"""
+function main()
+    time_end = 1000
+    startNode = 465
+    A = []
+    path = find_path(time_end,startNode,A)
+    plot_path(path,"ethantest.png",A)
+end
