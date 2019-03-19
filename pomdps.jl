68fa10a48bb6588eeb10a3e13a4adcba317d1699
tomer arnon <tomerarnon@gmail.com>
2019-03-19 09:13:49 -0700

    organize pomdps

--- pomdps.jl
+++ pomdps.jl
@@ -1,7 +1,6 @@
 
-
-
-
+using StaticArrays, LightGraphs, MetaGraphs, LazySets, Random, POMDPs, Combinatorics
+include("ndgrid.jl")
 #=
 S :: vector of agents and pos of Adversary
 A :: a rearrangement of the agents
@@ -18,48 +17,38 @@ R ::
     maximize agent utilization
 =#
 
-Vec2 = SVector{2, Int64}
-
-mutable struct SkyNet <: POMDP{SNState, SNAction, SNObs}
-    env::Environment
-    partition_points::Vector{Vector{Tuple}}
-    partitions::Vector{Region}
-    max_adversaries::Int8
-    intrusion_prob::Float64
-end
-
+const Vec2 = SVector{2, Int64}
+const AGENT_ACTIONS = [Vec2(i, j) for i in -1:1 for j in -1:1 if !(i==j==0)]
+############################################
+#########  Environment -related  ###########
+############################################
 struct Environment
     size::NTuple{2, Int}
     obstacles::Vector{Hyperrectangle}
     visibility::Vector{Vector{Tuple}}
     G::MetaGraph
+
+    function Environment(size::NTuple{2, Int}, obstacles::Vector{Hyperrectangle}, R::Int64)
+
+        G = create_graph(size...)
+        remove_obstacles(G, obstacles)
+
+        inf_vis = inf_visibile(G, obstacles)
+        visibility = limited_visible(G, inf_vis)
+
+        return new(size, obstacles, visibility, G)
+    end
 end
 
+Base.size(env::Environment) = env.size
 graph(env::Environment) = env.G
-graph(p::SkyNet) = p.env.G
 obstacles(env::Environment) = env.obstacles
-obstacles(p::SkyNet) = p.env.obstacles
 
 
-"""
-Sample a random position from a metagraph
-"""
-rand(G::AbstractGraph, rng::AbstractRNG = Random.GLOBAL_RNG) = rand(rng, 1:nv(G))
-
-
-
-abstract struct Agent end
-
-mutable struct Patroller <: Agent
-    pos::NTuple{2, Int}
-    R::Int64
-    partition::Region
-    state::Symbol # consider making enum
-end
-
-struct Adversary <: Agent
-    pos::NTuple{2, Int}
-    R::Int64
+Base.rand(G::AbstractGraph, rng::AbstractRNG = Random.GLOBAL_RNG) = rand(rng, 1:nv(G))
+function rand_pos(G::AbstractGraph, rng)
+    n = rand(G, rng)
+    get_pos_tup(G, n)
 end
 
 struct Region
@@ -67,70 +56,132 @@ struct Region
     interior::Vector{Tuple}
 end
 
+
+####################################################
+#################### S, A, O #######################
+####################################################
+mutable struct Agent
+    pos::Vec2
+    partition::Region
+end
+
+partition(ag::Agent) = ag.partition
+position(ag::Agent)  = ag.pos
+pos(ag::Agent)       = ag.pos
+
 struct SNState
-    patrollers::Vector{Vec2}
+    patrollers::Vector{Agent}
+    adversaries::Vector{Vec2}
+end
+SNAction = Union{Vector{Vec2}, Symbol}
+
+struct SNObs
     adversaries::Vector{Vec2}
 end
 
-agents(s::SNState) = (s.patrollers, s.adversaries)
+agents(s::SNState)      = (s.patrollers, s.adversaries)
+patrollers(s::SNState)  = s.patrollers
+adversaries(s::SNState) = s.adversaries
 
-# Just an alias:
-SNAction = Vector{Vec2}
+####################################################
+################ Adversary related #################
+####################################################
 
-function actions(p::SkyNet, s::SNState)
-    obstacles = p.env.obstacles
+abstract type AdversaryModel end
 
-    A = hcat(actions(p, pat) for pat in patrollers(s))'
+struct Random <: AdversaryModel end
+struct MaximizeDistance <: AdversaryModel end
+struct KnowsBelief{APB<:AbstractParticleBelief} <: AdversaryModel
+    belief::APB
+end
+# There was another one wasn't there?
+
+function heuristic_update(::MaximizeDistance, adv::Agent)
+
 end
 
-function actions(p::SkyNet, s::Vec2)
-    # consider making global
-    dirs = [Vec2(i, j) for i in -1:1 for j in -1:1 if !(i==j==0)]
+####################################################
+############# POMDP type + functions ###############
+####################################################
+mutable struct SkyNet <: POMDP{SNState, SNAction, SNObs}
+    env::Environment
+    partition_points::Vector{Vector{Tuple}} # maybe don't need
+    partitions::Vector{Region}              # maybe don't need
+    max_adversaries::Int8                   # maybe don't need
+    intrusion_prob::Float64                 # maybe don't need
+    R::Int64
+    adversarial_model::AdversaryModel
 
+    SkyNet() = new()
+end
+
+environment(p::SkyNet) = p.env
+graph(p::SkyNet) = p.env.G
+obstacles(p::SkyNet) = p.env.obstacles
+adversarial_model(p::SkyNet) = p.adversarial_model
+
+"""
+    isterminal checks if the nodes are outside the environment,
+    which is used as a flag for being terminal. Should only happen
+    if they were intentionally set that way at the end of generate_s,
+    if they chose to sound the alarm.
+"""
+isterminal(p::SkyNet, s::SNState) = any(pat->pos(pat) > size(environment(p)), patrollers(s))
+
+# TODO make it so that the alarm has to guess the right
+# position, otherwise periodic alarms always win,
+function actions(p::SkyNet, s::SNState)
+    agentwise_action_space = [actions(p, pat) for pat in patrollers(s)]
+    A = hcat.(vec.(ndgrid(agentwise_action_space...))...)
+    push!(A, :alarm)
+    return A
+end
+
+# maybe use neighbors instead of dir.
+function actions(p::SkyNet, s::Agent)
     A = eltype(action_type(SkyNet))[]
-    for dir in dirs
-        n_p = dir + s
-        if n_p ∈ s.partition && n_p ∈ p.env.G && all(n_p .∉ p.env.obstacles)
+    for dir in AGENT_ACTIONS
+        new_pos = dir + pos(s)
+        if new_pos ∈ partition(s) && all(new_pos .∉ obstacles(p))
             push!(A, dir)
         end
     end
     return A
 end
 
-struct SNObs
-    adversaries::Vector{Vec2}
-end
 
 
 
 
 
-Base.size(env::Environment) = env.size
+function generate_s(p::SkyNet, s::SNState, a::SNAction, rng::AbstractRNG)
 
+    if isterminal(s)
+        error("State is terminal in generate_s. Why is this happening? $s")
+        # return s
+    end
+    if a == :alarm
+        out_of_bounds_pats = patrollers(s) .+ Vec2(size(environment(p)))
+        return SNState(out_of_bounds_pats, adversaries(s))
+    end
 
-function compute_visitibility!(env::Environment)
-
-end
-
-
-function generate_s(p::SkyNet, s::SNState, a::Vector{Vec2}, rng::AbstractRNG)
     patrollers, adversaries = agents(s)
-
     updated_patrollers = patrollers .+ a
 
-    # With specified probability, create an adversary if there isn't one already.
-    if length(adversaries) < p.max_adversaries
-        if rand(rng) > p.intrusion_prob
-            push!(adversaries, Vec2(get_pos_tup(G, rand(G, rng))))
-        end
-    else
-        # update the adversaries' positions somehow
-    end
+    # update adversary's position somehow:
+    new_adv = heuristic_update(adversarial_model(p), adversaries)
+
+    return SNState(patrollers, adversaries)
 end
 
 
 function generate_o(p::SkyNet, s::SNState, rng::AbstractRNG)
     noise = rand(p.d, rng)
 
+
+end
+
+
+function reward(p::SkyNet, s::SNState, a::SNAction, sp::SNState)
 
 end
